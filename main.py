6b6d1349411299b0d635846f30e83ad51fd4543f
Firat <firatsahnn@gmail.com>
2023-11-01 23:57:59 +0300

    bug fix

--- main.py
+++ main.py
@@ -4,16 +4,20 @@ import numpy as np
 import tempfile
 import time
 import os
+import logging
 
 app = Flask(__name__)
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='error.log', level=logging.ERROR)
 
-sift = cv2.xfeatures2d.SIFT_create() #Initialize SIFT algorithm
 MIN_MATCH_COUNT = 30  #Minimum number of matches
+ratio_thresh = 0.75
 
-trainingImg_path = "img/2.jpeg"
-trainingImg = cv2.imread(trainingImg_path, 0)
+default_trainingImg_path = "img/2.jpeg"
+trainingImg = cv2.imread(default_trainingImg_path, 0)   
 
+sift = cv2.SIFT_create() #Initialize SIFT algorithm
 keypoints_1, descriptors_1 = sift.detectAndCompute(trainingImg, None) #detect and compute for the training img
 
 
@@ -40,36 +44,47 @@ def gen_frames(detect_frame): # Generate frames, detect keypoints, and compute d
 
                     img_matches = np.empty((0, 0))
                     good_matches = []
-                    for m, n in knn_matches:
-                        if m.distance < 0.75 * n.distance: # Filter and keep good matches
-                            good_matches.append(m)
+
+                    # Filter and keep good matches
+                    for i in range(len(knn_matches)):
+                        if knn_matches[i][0].distance < ratio_thresh * knn_matches[i][1].distance:
+                            good_matches.append(knn_matches[i][0])
                     
                     if len(good_matches) > MIN_MATCH_COUNT:
+                        tp = []
+                        qp = []
+                        for k in range(len(good_matches)):
+                            qp.append(keypoints_2[good_matches[k].trainIdx].pt)
+                            tp.append(keypoints_1[good_matches[k].queryIdx].pt)
 
-                        tp = np.float32([keypoints_1[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2) #Transform points from the training image
-                        qp = np.float32([keypoints_2[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2) #Transform points from the real-time frame
-
-                        H, _ = cv2.findHomography(tp, qp, cv2.RANSAC) #perspective transformation matrix
+                        H, _ = cv2.findHomography(np.array(tp), np.array(qp), cv2.RANSAC) #perspective transformation matrix
 
                         s = trainingImg.shape
-                        rows, cols = s[0], s[1]
+                        rows = s[0]
+                        cols = s[1]
 
-                        trainingBorder = np.array([[[0, 0], [cols - 1, 0], [cols - 1, rows - 1], [0, rows - 1]]], dtype='float32') #Border of the training image
-                        QueryBorder = cv2.perspectiveTransform(trainingBorder, H) #Transform the training image border using the found perspective transformation
+                        trainingBorder = np.array([[0, 0], [cols - 1, 0], [cols - 1, rows - 1], [0, rows - 1]], dtype=np.float32) #Border of the training image
+                        QueryBorder = cv2.perspectiveTransform(trainingBorder.reshape(1, -1, 2), H).reshape(-1, 2) #Transform the training image border using the found perspective transformation
                         cv2.polylines(frame, [np.int32(QueryBorder)], True, (0, 255, 0), 4) # Draw a polygon
-                    
+                        
+                        # img_matches = cv2.drawMatches(trainingImg, keypoints_1, frame, keypoints_2,good_matches,None)
+                        
 
-                        img_matches = cv2.drawMatches(trainingImg, keypoints_1, frame, keypoints_2, good_matches, None)
-                
-                if detect_frame == False:
-                    yield (b'--frame\r\n'
-                           b'Content-Type: image/jpeg\r\n\r\n' + cv2.imencode('.jpg', img_matches)[1].tobytes() + b'\r\n')
-                else :
+                        if detect_frame == False:
+                            img_matches = cv2.drawMatches(trainingImg, keypoints_1, Query, keypoints_2,
+                                    good_matches, img_matches, (0, 0, 255), (0, 0, 255),
+                                    [], cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+                                                
+                            yield (b'--frame\r\n'
+                                b'Content-Type: image/jpeg\r\n\r\n' + cv2.imencode('.jpg', img_matches)[1].tobytes() + b'\r\n')
+
+                if detect_frame == True:
                     yield (b'--frame\r\n'
                         b'Content-Type: image/jpeg\r\n\r\n' + cv2.imencode('.jpg', frame)[1].tobytes() + b'\r\n')
+
             else:
-                print("Break1")
                 pass
+            
 
 
 @app.route('/') #main/template endpoint
@@ -79,25 +94,27 @@ def index():
 
 @app.route('/upload_image', methods=['POST']) #image upload endpoint
 def upload_image():
-    global trainingImg
+    global trainingImg,keypoints_1, descriptors_1 
     if 'image' in request.files:
         image = request.files['image']
 
         temp_file = tempfile.NamedTemporaryFile(delete=False)
         image.save(temp_file) #save file temporarily
         temp_file_path = temp_file.name
-        
+       
         trainingImg = cv2.imread(temp_file_path, 0)
+        keypoints_1, descriptors_1 = sift.detectAndCompute(trainingImg, None) #detect and compute for the training img
         return ''
 
     else:
-        trainingImg = cv2.imread(trainingImg_path, 0)
+        trainingImg = cv2.imread(default_trainingImg_path, 0)
+        keypoints_1, descriptors_1 = sift.detectAndCompute(trainingImg, None) #detect and compute for the training img
         return ''
 
 
 @app.route('/video_feed') #for the image matching frame
 def video_feed():
-    if streaming:  
+    if streaming:
         return Response(gen_frames(detect_frame=False), mimetype='multipart/x-mixed-replace; boundary=frame')
     else:
         return ''
@@ -116,8 +133,8 @@ def start_stream():
     video_capture = cv2.VideoCapture(0)
     if not video_capture.isOpened():
         return "Camera Error!", 500
-    video_capture.set(3, 640) 
-    video_capture.set(4, 480)
+    # video_capture.set(3, 640) 
+    # video_capture.set(4, 480)
     time.sleep(0.1)  # Delay for 0.1 seconds
     global streaming
     streaming = True
@@ -131,9 +148,8 @@ def close():
     try:
         video_capture.release()
     except : pass
-
-    trainingImg = cv2.imread(trainingImg_path, 0)
+    
     return ''
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=4242,debug=True)
+    app.run(host='127.0.0.1',port=4242,debug=True)
